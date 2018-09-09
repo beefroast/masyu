c985c79ccb1734455663b3feec6526e29af428af
Benjamin Frost <benjaminfrost@benjamins-macbook-pro.local>
2018-09-09 12:01:33 +0930

    Initial Commit

---
+++ Mashuyu/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Mashuyu
+//
+//  Created by Benjamin Frost on 9/9/18.
+//  Copyright © 2018 Benjamin Frost. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
